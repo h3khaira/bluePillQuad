0ef06222a3f779c4e3b5bdd902cd0409e99163fb
h3khaira <harseerat19@gmail.com>
2022-01-27 21:42:58 -0500

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,29 @@
-#include <Wire.h>
-#include <Arduino.h>
+#include<Arduino.h>
+#include<Wire.h>
 
+int gyro_x, gyro_y, gyro_z;
+ 
 void setup() {
-  // initialize digital pin PC13 as an output.
-  pinMode(PC13, OUTPUT);
+  pinMode(PB3, OUTPUT);
+  Serial.begin(57600);
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B); //power management register
+  Wire.write(0x00); // waking up gyro
+  digitalWrite(PB4, HIGH);
+  Wire.endTransmission();
 }
-
-// the loop function runs over and over again forever
+ 
 void loop() {
-  digitalWrite(PC13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(250);              // wait for a second
-  digitalWrite(PC13, LOW);    // turn the LED off by making the voltage LOW
-  delay(250);              // wait for a second
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68,6);
+  gyro_x = Wire.read()<<8 | Wire.read();
+  gyro_y = Wire.read()<<8 | Wire.read();
+  gyro_z = Wire.read()<<8 | Wire.read();
+  Serial.print(gyro_x);
+  delay(250);
 }
