4c40178a0ea2a6be48d6790ea0800eece98f19ef
h3khaira <harseerat19@gmail.com>
2022-01-29 15:47:20 -0500

    added mpu6050 communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,27 @@
 #include<Arduino.h>
 #include<Wire.h>
 
-int gyro_x, gyro_y, gyro_z;
+int16_t gyro_x, gyro_y, gyro_z; //using int16 as mpu6050 returns a 16 bit two's complement value. using int16 will auto resolve it into a 16 bit number instead of 32 bit if you use int
 TwoWire Wire1(PB11, PB10);
 
 void setup() {
   Serial.begin(57600);
   pinMode(PB3, OUTPUT);
   digitalWrite(PB3, HIGH);
+  Wire1.setClock(400000);
   Wire1.begin();
   delay(250);
   
-  Wire1.beginTransmission(0x68);
-  Wire1.write(0x6B);
-  Wire1.write(0x00);
+  Wire1.beginTransmission(0x68); //MPU-6050 address
+  Wire1.write(0x6B); //Access power management register
+  Wire1.write(0x00); //wake up the gyro
   Wire1.endTransmission();
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  Wire1.beginTransmission(0x68);
-  Wire1.write(0x43);
+  Wire1.beginTransmission(0x68); //mpu-6050 address
+  Wire1.write(0x43); 
   Wire1.endTransmission();
   Wire1.requestFrom(0x68,6);
   gyro_x = Wire1.read()<<8 | Wire1.read();
