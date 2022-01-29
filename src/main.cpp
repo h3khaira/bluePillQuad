07f165c21bfde2f7a89a8ebab67195b8cf5d695f
h3khaira <harseerat19@gmail.com>
2022-01-29 15:32:12 -0500

    working config

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,35 @@
 #include<Wire.h>
 
 int gyro_x, gyro_y, gyro_z;
- 
+TwoWire Wire1(PB11, PB10);
+
 void setup() {
-  delay(2000);
+  Serial.begin(57600);
   pinMode(PB3, OUTPUT);
   digitalWrite(PB3, HIGH);
-  SerialUSB.begin(115200);
-  Wire.setClock(400000);
-  Wire.begin();
+  Wire1.begin();
   delay(250);
-  Wire.beginTransmission(0x68);
-  Wire.write(0x6B); //power management register
-  Wire.write(0x00); // waking up gyro
-  Wire.endTransmission();
+  
+  Wire1.beginTransmission(0x68);
+  Wire1.write(0x6B);
+  Wire1.write(0x00);
+  Wire1.endTransmission();
 }
- 
+
+// the loop function runs over and over again forever
 void loop() {
-  SerialUSB.println("helll0o");
-  delay(1000);
-  Wire.beginTransmission(0x68);
-  Wire.write(0x43);
-  Wire.endTransmission();
-  Wire.requestFrom(0x68,6);
-  gyro_x = Wire.read()<<8 | Wire.read();
-  gyro_y = Wire.read()<<8 | Wire.read();
-  gyro_z = Wire.read()<<8 | Wire.read();
-  SerialUSB.println("hello9");
+  Wire1.beginTransmission(0x68);
+  Wire1.write(0x43);
+  Wire1.endTransmission();
+  Wire1.requestFrom(0x68,6);
+  gyro_x = Wire1.read()<<8 | Wire1.read();
+  gyro_y = Wire1.read()<<8 | Wire1.read();
+  gyro_z = Wire1.read()<<8 | Wire1.read();
+  Serial.print("X = ");
+  Serial.print(gyro_x);
+  Serial.print(" Y = ");
+  Serial.print(gyro_y);
+  Serial.print(" Z = ");
+  Serial.println(gyro_z);
   delay(250);
 }
