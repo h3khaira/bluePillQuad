7894c575914718db196b131add17dea28de61dfe
h3khaira <harseerat19@gmail.com>
2022-01-29 14:17:51 -0500

    changing platformio settings

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,22 @@
 int gyro_x, gyro_y, gyro_z;
  
 void setup() {
+  delay(2000);
   pinMode(PB3, OUTPUT);
-  Serial.begin(57600);
+  digitalWrite(PB3, HIGH);
+  SerialUSB.begin(115200);
   Wire.setClock(400000);
   Wire.begin();
   delay(250);
   Wire.beginTransmission(0x68);
   Wire.write(0x6B); //power management register
   Wire.write(0x00); // waking up gyro
-  digitalWrite(PB4, HIGH);
   Wire.endTransmission();
 }
  
 void loop() {
+  SerialUSB.println("helll0o");
+  delay(1000);
   Wire.beginTransmission(0x68);
   Wire.write(0x43);
   Wire.endTransmission();
@@ -24,6 +27,6 @@ void loop() {
   gyro_x = Wire.read()<<8 | Wire.read();
   gyro_y = Wire.read()<<8 | Wire.read();
   gyro_z = Wire.read()<<8 | Wire.read();
-  Serial.print(gyro_x);
+  SerialUSB.println("hello9");
   delay(250);
 }
