20c9ddfc3f75b5b900d88dbaebf3b87ba3f51f9a
h3khaira <harseerat19@gmail.com>
2022-01-30 13:59:39 -0500

    stash

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include<Arduino.h>
 #include<Wire.h>
 
-int16_t gyro_x, gyro_y, gyro_z; //using int16 as mpu6050 returns a 16 bit two's complement value. using int16 will auto resolve it into a 16 bit number instead of 32 bit if you use int
-TwoWire Wire1(PB11, PB10);
-int gyroAddress = 0x68;
+int16_t gyroRoll, gyroPitch, gyroYaw; //using int16 as mpu6050 returns a 16 bit two's complement value. using int16 will auto resolve it into a 16 bit number instead of 32 bit if you use int
+int16_t gyroAddress = 0x68;
+int16_t temperature; 
+int16_t xAcc, yAcc, zAcc;
+
+TwoWire Wire1(PB11, PB10); //setting pb11 and pb10 and I2C data and clock signals
 
 void startGyro(){
   Wire1.beginTransmission(gyroAddress);
@@ -23,40 +26,46 @@ void startGyro(){
 
   Wire1.beginTransmission(gyroAddress);
   Wire1.write(0x1A); //Access CONFIG register
-  Wire1.write(0x06); // Digial Low Pass Filter for gyro/accelorometer set to 43 Hz
+  Wire1.write(0x06); //Digial Low Pass Filter for gyro/accelorometer set to 43 Hz
   Wire1.endTransmission();
+  digitalWrite(PB4, HIGH);
 }
 
 void readOrientation(){
   Wire1.beginTransmission(gyroAddress);
   Wire1.write(0x3B); //first register of accelerometer
   Wire1.endTransmission();
-  Wire1.requestFrom(gyroAddress,14); //read 14 bytes of information starting from 0x3B registerWire1
+  Wire1.requestFrom(gyroAddress,14); //read 14 bytes of information starting from 0x3B register
   while (Wire1.available() < 14); //waiting for information to be delivered
+  xAcc = Wire1.read() << 8 || Wire1.read(); // shift high byte left and add to low byte
+  yAcc = Wire1.read() << 8 || Wire1.read();
+  zAcc = Wire1.read() << 8 || Wire1.read();
+  temperature = Wire1.read() << 8 || Wire1.read();
+  // temperature = temperature/340 + 36.53;
+  gyroRoll = Wire1.read() << 8 || Wire1.read();
+  gyroPitch = Wire1.read() << 8 || Wire1.read();
+  gyroYaw = Wire1.read() << 8 || Wire1.read();
 }
 
 void setup() {
   // delay(4000);
   Serial.begin(57600);
+  pinMode(PB4, OUTPUT);
   pinMode(PB3, OUTPUT);
-  digitalWrite(PB3, HIGH);
   Wire1.begin();
   delay(250);
   Wire1.setClock(400000);
-  Serial.println("Talking to gyro"); 
   startGyro();
+  digitalWrite(PB3, HIGH);
 }
 
 void loop() {
   readOrientation();
-  gyro_x = Wire1.read()<<8 | Wire1.read();
-  gyro_y = Wire1.read()<<8 | Wire1.read();
-  gyro_z = Wire1.read()<<8 | Wire1.read();
   Serial.print("X = ");
-  Serial.print(gyro_x);
+  Serial.print(gyroRoll);
   Serial.print(" Y = ");
-  Serial.print(gyro_y);
+  Serial.print(yAcc);
   Serial.print(" Z = ");
-  Serial.println(gyro_z);
-  delay(250);
+  Serial.println(zAcc);
+  delay(1000);
 }
