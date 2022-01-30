7aaebe624ccf3a77bce22b2853f16f116396e2d1
h3khaira <harseerat19@gmail.com>
2022-01-30 18:04:30 -0500

    implemented MPU6050 pitch and roll measurements

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include<Arduino.h>
 #include<Wire.h>
 
-int16_t gyroRoll, gyroPitch, gyroYaw; //using int16 as mpu6050 returns a 16 bit two's complement value. using int16 will auto resolve it into a 16 bit number instead of 32 bit if you use int
-int16_t gyroAddress = 0x68;
-int rawTemp; //mpu6050 outputs 16 bit signed value
+//using int16 as mpu6050 returns a 16 bit two's complement value. using int16 will auto resolve it into a 16 bit number instead of 32 bit if you use int
+int16_t rawGyroRoll, rawGyroPitch, rawGyroYaw; //gyroscope measurement, resolution based on value of 1B register
+float rollCal = -298, pitchCal = -175, yawCal = 230; //for calibration ofgyro measurements
+int16_t gyroAddress = 0x68; // MPU-6050 I2C address
+int16_t rawTemp;
 float temperature;
-int16_t xAcc, yAcc, zAcc;
+int16_t rawXAcc, rawYAcc, rawZAcc; //Raw gyro acceleration values 4096 least significant bits per 9.81 m/s^2
+float accRoll, accPitch; //to store actual acceleration numbers
+float roll, pitch, yaw;
+uint8_t sampleRate; //unsigned 8 bit 
+
+float mpuFilterWeight = 0.96;
 
 TwoWire Wire1(PB11, PB10); //setting pb11 and pb10 and I2C data and clock signals
 
@@ -17,35 +24,54 @@ void startGyro(){
 
   Wire1.beginTransmission(gyroAddress);
   Wire1.write(0x1B); //Access the GYRO_CONFIG register
-  Wire1.write(0x08); //Write the FS_SEL bits to change gyro resolution to 500 degrees per second, 1 DEG/SEC  = 65.5 OUTPUT FROM GYRO LSB
+  Wire1.write(0x08); //Write the FS_SEL bits to change gyro resolution to 500 degrees per second, 1 DEG/SEC  = 65.5 OUTPUT FROM GYRO LSB (check page 31 of manual)
   Wire1.endTransmission();
 
   Wire1.beginTransmission(gyroAddress);
   Wire1.write(0x1C); //Access accelerometer config register
-  Wire1.write(0x10); // +-8g = 4096 LSB/g
+  Wire1.write(0x10); //Set the AFSL bit +-8g = 4096 LSB/g
   Wire1.endTransmission();
 
   Wire1.beginTransmission(gyroAddress);
   Wire1.write(0x1A); //Access CONFIG register
-  Wire1.write(0x06); //Digial Low Pass Filter for gyro/accelorometer set to 43 Hz
+  Wire1.write(0x03); //Digial Low Pass Filter for gyro/accelorometer set to 43 Hz
   Wire1.endTransmission();
   digitalWrite(PB4, HIGH);
 }
 
+void eulerAngles(){
+  accRoll = 57.2958 * atan2(rawYAcc, rawZAcc);
+  accPitch = 57.2958 * atan2(rawXAcc, sqrt(pow(rawYAcc,2) +pow(rawZAcc,2)));
+}
+
 void readOrientation(){
   Wire1.beginTransmission(gyroAddress);
-  Wire1.write(0x43); //first register of accelerometer
+  Wire1.write(0x3B); //first register of accelerometer
   Wire1.endTransmission();
   Wire1.requestFrom(gyroAddress,14); //read 14 bytes of information starting from 0x3B register
   while (Wire1.available() < 14); //waiting for information to be delivered
-  xAcc = Wire1.read() << 8 | Wire1.read(); // shift high byte left and add to low byte
-  yAcc = Wire1.read() << 8 | Wire1.read();
-  zAcc = Wire1.read() << 8 | Wire1.read();
+  rawXAcc = Wire1.read() << 8 | Wire1.read(); // shift high byte left and add to low byte
+  rawYAcc = Wire1.read() << 8 | Wire1.read();
+  rawZAcc = Wire1.read() << 8 | Wire1.read();
   rawTemp = Wire1.read() << 8 | Wire1.read();
-  // temperature = rawTemp/340 + 36.53;
-  gyroRoll = Wire1.read() << 8 | Wire1.read();
-  gyroPitch = Wire1.read() << 8 | Wire1.read();
-  gyroYaw = Wire1.read() << 8 | Wire1.read();
+  // temperature = rawTemp/340.0 + 36.53; //uncomment this to read temperature
+  rawGyroRoll = Wire1.read() << 8 | Wire1.read();
+  rawGyroPitch = Wire1.read() << 8 | Wire1.read();
+  rawGyroYaw = Wire1.read() << 8 | Wire1.read();
+
+//calibrating raw gyro values based on offsets measured in setup
+  rawGyroPitch -= pitchCal;
+  rawGyroRoll -= rollCal;
+  rawGyroYaw -= yawCal;
+
+  eulerAngles();
+
+// 0.000061 = 1/250/65.5 
+  roll += rawGyroRoll * 0.0000611; //integrating gyroscope velocity measurements by using sampling rate
+  pitch += rawGyroPitch * 0.0000611;
+
+  roll = mpuFilterWeight * roll + (1-mpuFilterWeight)* accRoll; //combining accelerometer and gyroscope measurements
+  pitch = mpuFilterWeight * pitch + (1-mpuFilterWeight)* pitch; //combining accelerometer and gyroscope measurements
 }
 
 void setup() {
@@ -58,14 +84,8 @@ void setup() {
 
 void loop() {
   readOrientation();
-  Wire1.read() << 8 | Wire1.read();
-  Serial.print("X = ");
-  Serial.print(xAcc);
-  Serial.print(" Y = ");
-  Serial.print(yAcc);
-  Serial.print(" Z = ");
-  Serial.println(zAcc);
-  Serial.print("Temperature is ");
-  Serial.println(rawTemp);
-  delay(250);
-}
+  Serial.print("Roll = ");
+  Serial.print(roll);
+  Serial.print(" Pitch = ");
+  Serial.println(pitch);
+  }
